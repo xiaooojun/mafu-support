4bf8d69b83e6bfe89d73eef3f24567c88e976e56
xiaojun <961721716@qq.com>
2025-10-18 15:03:38 +0800

    Add support page for MaFu app

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by tangxj on 10/16/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+    }
+}
